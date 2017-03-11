213541322200e211171906b4235d7d55fc06233b
chaozhiguang <646869341@qq.com>
2017-03-11 11:42:07 +0800

    靠尼玛

--- app/src/main/java/com/meng/interest/lotteryturntable/ui/MainActivity.java
+++ app/src/main/java/com/meng/interest/lotteryturntable/ui/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         initViews();
 
+
+
     }
 
     private void initViews() {
